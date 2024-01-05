b26063707fa36faba8ed9a249e977fc4e6a872b2
sschonss <schonsluuiz@gmail.com>
2024-01-05 15:14:15 -0300

    Update Project with Verify OS

--- main.go
+++ main.go
@@ -8,27 +8,41 @@ import (
 )
 
 func printImage(w http.ResponseWriter, r *http.Request) {
-    err := r.ParseForm()
-    if err != nil {
-        log.Println(err)
-        fmt.Fprintf(w, "Error parsing form: %s\n", err)
-        return
-    }
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, "Error parsing form: %s\n", err)
+		return
+	}
 
-    imageName := r.FormValue("image")
-    copies := r.FormValue("copies")
+	imageName := r.FormValue("image")
+	copies := r.FormValue("copies")
+	os := r.FormValue("os")
 
-    // Execute the Bash script with the provided parameters
-    cmd := exec.Command("bash", "autoprinter.sh", imageName, copies)
-    output, err := cmd.CombinedOutput()
-    if err != nil {
-        log.Println(err)
-        fmt.Fprintf(w, "Error printing image: %s\n", err)
+	fmt.Fprintf(w, "Printing image %s %s times...\n", imageName, copies)
+
+	var cmd *exec.Cmd
+
+	if os == "linux" {
+		fmt.Println("Linux detected")
+		cmd = exec.Command("bash", "autoprinter.sh", imageName, copies)
+	} else if os == "win" {
+		fmt.Println("Windows detected")
+		cmd = exec.Command("powershell", "autoprinter.ps1", imageName, copies)
+	} else {
+		cmd = exec.Command("echo", "OS not detected")
+		fmt.Println("OS not detected")
+	}
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, "Error printing image: %s\n", err)
 		fmt.Fprintf(w, "Output: %s\n", output)
-        return
-    }
-    
-    fmt.Fprintf(w, "Print request processed successfully.\n%s\n", output)
+		return
+	}
+
+	fmt.Fprintf(w, "Print request processed successfully.\n%s\n", output)
 }
 
 func explainAPI(w http.ResponseWriter, r *http.Request) {
@@ -38,16 +52,17 @@ func explainAPI(w http.ResponseWriter, r *http.Request) {
 		To print an image, send a POST request to /print with the following parameters:
 		- image: Image name
 		- copies: Number of copies
+		- os: Operating System ("linux" or "win")
 
 		Example:
-		curl -X POST -d "image=123456789&copies=2" http://localhost:8080/print
+		curl -X POST -d "image=123456789&copies=2&os=linux" http://localhost:8080/print
 	`
 	fmt.Fprintln(w, explanation)
 }
 
 func main() {
 	fmt.Println("Starting server...")
-	http.HandleFunc("/", explainAPI) 
+	http.HandleFunc("/", explainAPI)
 	http.HandleFunc("/print", printImage)
 	fmt.Println("Listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
