e2523a3a652eda4e7800db2e93d6b94d0be54703
sschonss <schonsluuiz@gmail.com>
2024-01-05 15:51:33 -0300

    Update printing method in autoprinter.ps1

--- autoprinter.ps1
+++ autoprinter.ps1
@@ -19,7 +19,7 @@ $printerName = $printer.Name
 Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss'): Starting printing of $num_copies copies of file '$image_file' on printer '$printerName'"
 
 for ($i = 0; $i -lt $num_copies; $i++) {
-    Get-Content $image_file | Out-Printer -Name $printerName
+    Start-Process -FilePath "C:\Windows\System32\rundll32.exe" -ArgumentList "C:\Windows\System32\shimgvw.dll,ImageView_PrintTo /pt `"$printerName`" `"$image_file`""
 }
 
 Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss'): Printing of $num_copies copies of file '$image_file' on printer '$printerName' completed"
